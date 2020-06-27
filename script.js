b90d55d639a10b74c18e1a1610ac56b4a80c17d6
marcosdellavecchia <33458523+marcosdellavecchia@users.noreply.github.com>
2020-06-26 21:56:53 -0300

    Update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const clearBtn = document.querySelector('.btn-clear');
 
 // Recorre todos los botones con un for para agregarles un event listener
 // Asigna el atributo de cada boton a la variable number
-// Agrega a screen los valores de number
+// Agrega al valor de screen los atributos de number
 for(let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function() {
         let number = btns[i].getAttribute('data-num');
@@ -14,7 +14,7 @@ for(let i = 0; i < btns.length; i++) {
 }
 
 // Se define una variable que evalua el valor en screen y ejecuta la operacion. 
-// Se crea un if para que solo evalue si hay valores en pantalla
+// Se crea un if para que unicamente evalue si es que hay valores en pantalla.
 equalBtn.addEventListener('click', function() {
     if(screen.value === '') {
         alert('Ingresa un valor')
